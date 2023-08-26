744bb525f92d1815e6546f9957e2cbe9f063f929
parkrootseok <parkrootseok@gmail.com>
2023-08-26 18:14:54 +0900

    Init: 프로젝트 시작
    - .gitignore 작성 완료
    - 사용할 dependency 추가 완료

---
+++ src/main/java/dev/yogizogi/YogizogiApplication.java
@@ -0,0 +1,13 @@
+package dev.yogizogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogizogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YogizogiApplication.class, args);
+	}
+
+}
