ed49b0f4f086eea663c11ee1c44a80b30e30a343
parkrootseok <parkrootseok@gmail.com>
2023-09-06 11:43:08 +0900

    Style: Validation Result (#1)
    - 양식 수정

--- src/main/java/dev/yogizogi/global/common/constant/Format.java
+++ src/main/java/dev/yogizogi/global/common/constant/Format.java
@@ -2,7 +2,7 @@ package dev.yogizogi.global.common.constant;
 
 public class Format {
 
-    public static final String VALIDATION_RESULT = "error message : %s / field : [%s] / input : [%s]";
+    public static final String VALIDATION_RESULT = "Error message : [%s] / Field : [%s] / Input : [%s]";
     public static final String VERIFICATION_CODE_MESSAGE = "[YOGIZOGI]\n 아래의 인증번호를 입력해주세요.\n";
 
 }
