aab409409b927ce7f4fa60b045409a4c5a469e82
parkrootseok <parkrootseok@gmail.com>
2023-09-08 18:31:10 +0900

    Feat: 로그인 기능 구현 (#7)

--- src/main/java/dev/yogizogi/global/common/model/response/Success.java
+++ src/main/java/dev/yogizogi/global/common/model/response/Success.java
@@ -5,12 +5,13 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Success<T> implements Result {
 
-    private int code;
+    private HttpStatus code;
     private String message;
     private T data;
 
