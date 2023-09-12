1ad30424005e628d8ef275b3183f5903b1bbe815
parkrootseok <parkrootseok@gmail.com>
2023-09-12 21:09:48 +0900

    Refactor: 회원가입 API (#12)
    - AuthApiController.java로 이동

--- src/main/java/dev/yogizogi/domain/member/api/MemberApiController.java
+++ src/main/java/dev/yogizogi/domain/member/api/MemberApiController.java
@@ -1,17 +1,8 @@
 package dev.yogizogi.domain.member.api;
 
-import dev.yogizogi.domain.member.model.dto.request.CreateMemberInDto;
 import dev.yogizogi.domain.member.service.MemberService;
-import dev.yogizogi.global.common.model.response.Result;
-import dev.yogizogi.global.common.model.response.Success;
-import dev.yogizogi.global.util.ResponseUtils;
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,16 +13,5 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberApiController {
 
     private final MemberService memberService;
-
-    @Operation(summary = "회원 가입")
-    @PostMapping("/sign-up")
-    public ResponseEntity createMember(@RequestBody @Valid CreateMemberInDto response) {
-
-        return ResponseUtils.created(
-                Success.builder()
-                        .data(memberService.signUp(response))
-                        .build());
-
-    }
 
 }
