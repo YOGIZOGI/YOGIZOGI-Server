d53fcea3c0a2625d315dfe3627a61b4b4c7b5361
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-11-06 23:44:07 +0900

    Test: 메뉴 생성 비즈니스 로직, 컨트롤러 (#57)

---
+++ src/test/java/dev/yogizogi/domain/menu/factory/dto/CreateMenuFactory.java
@@ -0,0 +1,21 @@
+package dev.yogizogi.domain.menu.factory.dto;
+
+import static dev.yogizogi.domain.menu.factory.fixtures.MenuFixtures.가격;
+import static dev.yogizogi.domain.menu.factory.fixtures.MenuFixtures.설명;
+import static dev.yogizogi.domain.menu.factory.fixtures.MenuFixtures.음식_사진;
+import static dev.yogizogi.domain.menu.factory.fixtures.MenuFixtures.음식명;
+
+import dev.yogizogi.domain.menu.model.dto.request.CreateMenuInDto;
+import dev.yogizogi.domain.menu.model.dto.response.CreateMenuOutDto;
+
+public class CreateMenuFactory {
+
+    public static CreateMenuInDto createMenuInDto() {
+        return new CreateMenuInDto(1L, 음식명, 가격, 설명, 음식_사진);
+    }
+
+    public static CreateMenuOutDto createMenuOutDto() {
+        return CreateMenuOutDto.of(음식명);
+    }
+
+}
