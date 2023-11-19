2eb71959e4157c1b7e54c16b15edfdd987d619e6
parkrootseok <86178336+parkrootseok@users.noreply.github.com>
2023-11-19 22:36:40 +0900

    Feat: 지도에 음식점 추가 (#71)

---
+++ src/main/java/dev/yogizogi/domain/map/model/entity/Map.java
@@ -0,0 +1,40 @@
+package dev.yogizogi.domain.map.model.entity;
+
+import dev.yogizogi.domain.restaurant.model.entity.Restaurant;
+import dev.yogizogi.domain.user.model.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Map {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @Builder
+    public Map(User user, Restaurant restaurant) {
+        this.user = user;
+        this.restaurant = restaurant;
+    }
+
+}
